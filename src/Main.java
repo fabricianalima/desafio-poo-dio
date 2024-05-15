8de769228a203d53f02c872387fe7b271992226b
fabricianalima <fabricianacorreialima@gmail.com>
2024-05-15 17:11:20 -0300

    Criação das classes Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        Curso curso2 = new Curso();
+
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        curso2.setTitulo("curso javascript");
+        curso2.setDescricao("descrição curso Javascript");
+        curso2.setCargaHoraria(6);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descrição mentoria de Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+    }
+}
